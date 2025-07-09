f860a4f4da30f6349d6f4d581e108842cd52d9c6
Shahna-20 <shahnaak123@gmail.com>
2025-07-09 23:36:19 +0530

    allow origin

--- shoppit/settings.py
+++ shoppit/settings.py
@@ -68,6 +68,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:5175",
+    "https://shoppit-1mgt.onrender.com"
 ]
 
 ROOT_URLCONF = 'shoppit.urls'
